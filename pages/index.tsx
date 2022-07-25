97228d19d7901603084983b64f93d76811b177e1
johnmiicheal <michealelijah301@gmail.com>
2022-07-25 16:16:00 +0100

    better-responsive dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,8 +118,69 @@ const Home: NextPage = () => {
           <WelcomeScreen />
         ) : (
           <Layout page={page} setPage={setPage}>
-            <Flex direction='row' justify='space-between' align='center'>
-              <Flex direction='column'>
+            <Flex direction={{base: 'column' , lg: 'row'}} justify='space-between' align='center' w={{ base: 'full', md: 'full' }}>
+            <Box
+                w='full'
+                bgImg="/swisright.png"
+                top="0"
+                mb={10}
+                pb={2}
+                borderRadius='md'
+                display={{ base: 'block', lg: 'none'}}
+              >
+                <Flex px={3} mt={2} direction="row" justify="space-between">
+                
+                  <Flex direction="column">
+                    <Flex align='center'>
+                    <Heading
+                      color="white"
+                      fontWeight={600}
+                      fontSize={20}
+                      mb={2}
+                      mr={7}
+                    >
+                      {process.env.NEXT_PUBLIC_FULL_NAME}
+                    </Heading>
+                    <Text color="white" fontWeight={400} fontSize={16}>
+                      {process.env.NEXT_PUBLIC_YOUR_LOCATION}
+                    </Text>
+                    </Flex>
+                  <Text color="white" fontWeight={600} fontSize={30} mt={5} >
+                    {" "}
+                    {time}{" "}
+                  </Text>
+
+                  </Flex>
+
+                </Flex>
+                <Box px={4}>
+                <Box bg='white' borderRadius='md'>
+                <Flex direction="row" mt={10} align='center'>
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_REST_ENDPOINT}/weather_img`}
+                    alt={weatherData?.label}
+                    w={24}
+                  />
+                    <Heading color="#000A16" fontWeight={500}>
+                      {" "}
+                      {weatherData?.ext_temp}º C{" "}
+                    </Heading>
+                </Flex>
+                  <Flex mx={8} align="center">
+                    <Text color="#000A16" fontWeight={400} mt={-5}>
+                      {" "}
+                      {(weatherData?.label || "").replace(
+                        /(^\w{1})|(\s+\w{1})/g,
+                        (letter) => letter.toUpperCase()
+                      )}{" "}
+                    </Text>
+                  </Flex>
+                  </Box>
+                  </Box>
+
+              </Box>
+
+              <Flex direction='column' ml={{ base: 10 }}>
             {page === "/" ? 
             <>
               <Heading fontSize={24} mt={2} mb={5}>
@@ -134,6 +195,7 @@ const Home: NextPage = () => {
                 w={{base: "350px", md: "680px"}} 
                 h={{ base: "200px", md: "300px"}} 
                 mt={10}
+                mb={10}
                 borderRadius='md' 
                 borderWidth='2px' 
                 borderColor='gray.300' 
@@ -141,6 +203,11 @@ const Home: NextPage = () => {
                 <iframe 
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.2199119983507!2d3.8918724396182944!3d7.4409033119709305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3585d348366907b9!2zN8KwMjYnMjcuOCJOIDPCsDUzJzMxLjYiRQ!5e0!3m2!1sen!2sng!4v1657699637955!5m2!1sen!2sng" width="600" height="450" allowFullScreen loading="lazy"  />
             </AspectRatio>
+
+            <Flex direction={{base: 'column' , md: 'row'}} mb={5}>
+            <GasResistanceWidget gasResistance={gasResistance/100}/>
+            <PrecipitationWidget data={precipitation} />
+            </Flex>
             
             </>
             : <></>}
