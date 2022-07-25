18bf1c2295e5f744fa1c1678ecfa422bfc15020e
johnmiicheal <michealelijah301@gmail.com>
2022-07-25 15:47:55 +0100

    fixed responsive issues

--- components/WeatherWidget.tsx
+++ components/WeatherWidget.tsx
@@ -1,8 +1,5 @@
-import { Flex, Alert, AlertIcon, AlertTitle, Box, Heading, SimpleGrid, Image, Text } from "@chakra-ui/react";
+import { Flex, Alert, AlertIcon, AlertTitle, Box, Heading, SimpleGrid, Image, Text, Stat, StatGroup, StatNumber, StatHelpText, StatArrow, StatLabel } from "@chakra-ui/react";
 import {InfoIcon} from '@chakra-ui/icons'
-
-import { useBreakpointValue } from '@chakra-ui/react'
-import { time } from "console";
 import { useState, useEffect } from "react";
 
 export default function WeatherWidget({data, uv}: {data: any, uv:number}){
@@ -61,34 +58,34 @@ export default function WeatherWidget({data, uv}: {data: any, uv:number}){
         //     }}>Weather Widget</h4>
         // </Container>
 
-        <Flex justifyContent="space-between">
-              <SimpleGrid
-                columns={{ base: 1, md: 2, lg: 2 }}
-                spacing={{ base: 10, md: 10 }}
-                mt={10}
-                ml={-16}
-              >
+        <Flex>
+          <Flex direction='column' >
+              <Flex direction='row' mb={5}>
                 <Box
                   bg="gray.300"
-                  pl={2}
                   pr={4}
                   w={80}
                   h={28}
                   borderRadius="md"
+                  mr={5}
                 >
                   <Flex direction="row" align="center" mt={2}>
-                    <Image src="/wind.png" alt="wind_icon" w={16} mr={5} />
-                    <Flex direction="column">
-                      <Text fontWeight={400} fontSize={14} color="gray.500">
-                        Wind Speed
-                      </Text>
-                      <Heading fontWeight={500}>
-                        {data.wind_speed}km/h
-                      </Heading>
-                      <Text mt={2} >Direction: {data.wind_direction} </Text>
-                    </Flex>
-                    
+                    <Image src="/wind.png" alt="wind_icon" w={16} mr={5} pl={2}/>
+                      <Stat>
+                        <StatLabel>Wind Speed</StatLabel>
+                        <StatNumber>{data.wind_speed}km/h</StatNumber>
+                        <StatHelpText>
+                          <StatArrow type="decrease" />
+                          1.9%
+                        </StatHelpText>
+                        </Stat>   
                   </Flex>
+                        <Box bg='gray.400' w={80} borderRadius="0 0 7px 7px" boxShadow='md'
+>
+                          <Text px={2}>
+                          Wind Direction: {data.wind_direction}
+                          </Text>
+                          </Box>                 
                 </Box>
 
                 <Box
@@ -96,29 +93,36 @@ export default function WeatherWidget({data, uv}: {data: any, uv:number}){
                   pl={2}
                   pr={4}
                   w={80}
-                  h={20}
+                  h={28}
                   borderRadius="md"
+                  boxShadow='md'
+
                 >
                   <Flex direction="row" align="center" mt={2}>
                     <Image src="/barometer.png" alt="pressure" w={16} mr={5} />
-                    <Flex direction="column">
-                      <Text fontWeight={400} fontSize={14} color="gray.500">
-                        Pressure
-                      </Text>
-                      <Heading fontWeight={500}>
-                        {data.baro_pressure}psi
-                      </Heading>
-                    </Flex>
+                    <Stat>
+                        <StatLabel>Pressure</StatLabel>
+                        <StatNumber>{data.baro_pressure}psi</StatNumber>
+                        <StatHelpText>
+                          <StatArrow type="increase" />
+                          3.4%
+                        </StatHelpText>
+                        </Stat>
                   </Flex>
                 </Box>
-
+              </Flex>
+              
+              <Flex>
                 <Box
                   bg="gray.300"
                   pl={2}
                   pr={4}
                   w={80}
                   h={28}
+                  mr={5}
                   borderRadius="md"
+                  boxShadow='md'
+
                 >
                   <Flex direction="row" align="center" mt={2}>
                     <Image src="/sun.png" alt="uv_index" w={16} mr={5} />
@@ -129,7 +133,7 @@ export default function WeatherWidget({data, uv}: {data: any, uv:number}){
                       <Heading fontWeight={500}>{uv}</Heading>
                     </Flex>
                   </Flex>
-                  <Flex backgroundColor={uvColor} borderRadius="md" mt={2} ml={-2} w={80} p={5}>
+                  <Flex backgroundColor={uvColor} borderRadius="0 0 7px 7px" h={8} mt={2} ml={-2} w={80} align='center' px={2}>
         
                     <InfoIcon fontSize={18} /> &nbsp;
                     UV Level: &nbsp; <b>{uv <= 2 ? "Low" : uv <= 5? "Medium": 
@@ -142,31 +146,35 @@ export default function WeatherWidget({data, uv}: {data: any, uv:number}){
                   pl={2}
                   pr={4}
                   w={80}
-                  h={20}
+                  h={28}
                   borderRadius="md"
+                  boxShadow='md'
                 >
                   <Flex direction="row" align="center" mt={2}>
                     <Image src="/humidity.png" alt="humidity" w={16} mr={5} />
-                    <Flex direction="column">
-                      <Text fontWeight={400} fontSize={14} color="gray.500">
-                        Humidity
-                      </Text>
-                      <Heading fontWeight={500}>
-                        {data.humidity}%
-                      </Heading>
-                    </Flex>
+                      <Stat>
+                        <StatLabel>Humidity</StatLabel>
+                        <StatNumber>{data.humidity}%</StatNumber>
+                        <StatHelpText>
+                          <StatArrow type="increase" />
+                          5.5%
+                        </StatHelpText>
+                        </Stat>
                   </Flex>
                 </Box>
+              </Flex>
+
+
+
               {/* <Flex>
                 <DataView />
                 <Flex justify="center" mt={20} pb={20} borderRadius={20}>
                   <MapWidget />
                 </Flex>
               </Flex> */}
-              </SimpleGrid>
 
 
-             
+</Flex>
             </Flex>
     )
 }
